29bbc080a16c1ed54ce49894484976887d0d84eb
Jing <n2jeam@sina.com>
2023-02-04 01:54:43 -0600

    remove unnecessary libs

--- ToothTray/TrayIcon.h
+++ ToothTray/TrayIcon.h
@@ -4,11 +4,6 @@
 #include <shellapi.h>
 #include <windowsx.h>
 
-#include <commctrl.h>
-#pragma comment(lib, "comctl32.lib")
-// necessary for Shell_NotifyIcon
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 #include "debuglog.h"
 
 class TrayIcon {
